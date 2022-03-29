ea719b5f8e7cb7253f5c4788fefbc1515ed83832
Vinicius <vinipanjos@hotmail.com>
2022-03-28 21:25:09 -0300

    Criação do layout da tela de login

---
+++ app/src/main/java/com/vinicius/instagram/login/view/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.vinicius.instagram.login.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vinicius.instagram.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
